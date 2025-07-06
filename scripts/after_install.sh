faf66354a4be20678d509a4513a040cca191413b
anveshmuppeda <muppedaanvesh@gmail.com>
2025-07-06 16:42:39 -0500

    Fix typo in footer text and update after_install.sh script for consistent CICD terminology

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,5 @@ EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_INSTANCE_TYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
 EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 
-sed -i "s/Application successfully deployed via AWS CI/CD Pipeline/This is currently runnining on $EC2_INSANCE_TYPE with id - $EC2_INSTANCE_ID on AZ - $EC2_AZ/g" /var/www/html/index.html
+sed -i "s/Application successfully deployed via AWS CICD Pipeline/This is currently runnining on $EC2_INSANCE_TYPE with id - $EC2_INSTANCE_ID on AZ - $EC2_AZ/g" /var/www/html/index.html
 chmod 664 /var/www/html/index.html
