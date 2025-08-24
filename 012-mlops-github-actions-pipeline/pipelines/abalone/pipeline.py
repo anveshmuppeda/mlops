646f381fb60e14b8241b539ab15eaaaf8a021e0d
anveshmuppeda <muppedaanvesh@gmail.com>
2025-08-24 14:02:04 -0500

    fix: change training instance type from ml.t3.medium to ml.t2.medium in abalone pipeline

--- 012-mlops-github-actions-pipeline/pipelines/abalone/pipeline.py
+++ 012-mlops-github-actions-pipeline/pipelines/abalone/pipeline.py
@@ -129,7 +129,7 @@ def get_pipeline(
     pipeline_name="AbalonePipeline",
     base_job_prefix="Abalone",
     processing_instance_type="ml.t3.medium",
-    training_instance_type="ml.t3.medium",
+    training_instance_type="ml.t2.medium",
 ):
     """Gets a SageMaker ML Pipeline instance working with on abalone data.
 
